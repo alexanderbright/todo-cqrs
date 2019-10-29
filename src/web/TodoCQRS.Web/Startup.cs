b69e5ce42284800033aef034d199f2f4b74193f9
alexanderbright <mr.alex_1@mail.ru>
2019-10-30 00:10:15 +0200

    .net core 3.0 stable

--- src/web/TodoCQRS.Web/Startup.cs
+++ src/web/TodoCQRS.Web/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using TodoCQRS.Infrastructure.Framework;
 using TodoCQRS.Infrastructure.MsSql.Repositories;
@@ -17,66 +18,72 @@ using TodoCQRS.Web.Application.Hubs;
 
 namespace TodoCQRS.Web
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-    public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public IServiceProvider ConfigureServices(IServiceCollection services)
-    {
-      services.AddSignalR().AddNewtonsoftJsonProtocol();
-      services.AddMvc(option => option.EnableEndpointRouting = false).AddNewtonsoftJson();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR().AddNewtonsoftJsonProtocol();
+            services.AddMvc(option => option.EnableEndpointRouting = false).AddNewtonsoftJson();
 
-      return new Container(rules => rules.WithoutThrowIfDependencyHasShorterReuseLifespan())
-        // optional: support for MEF service discovery
-        //.WithMef()
-        // setup DI adapter
-        .WithDependencyInjectionAdapter(services,
-          // optional: get original DryIoc.ContainerException if specified type is not resolved, 
-          // and prevent fallback to default resolution by infrastructure
-          throwIfUnresolved: type => type.Name.EndsWith("Controller"),
+            return new Container(rules => rules.WithoutThrowIfDependencyHasShorterReuseLifespan())
+              // optional: support for MEF service discovery
+              //.WithMef()
+              // setup DI adapter
+              .WithDependencyInjectionAdapter(services,
+                // optional: get original DryIoc.ContainerException if specified type is not resolved, 
+                // and prevent fallback to default resolution by infrastructure
+                throwIfUnresolved: type => type.Name.EndsWith("Controller"),
 
-          // optional: You may Log or Customize the infrastructure components registrations
-          registerDescriptor: (registrator, descriptor) =>
-          {
+                // optional: You may Log or Customize the infrastructure components registrations
+                registerDescriptor: (registrator, descriptor) =>
+                {
 #if DEBUG
-            if (descriptor.ServiceType == typeof(ILoggerFactory))
-              Console.WriteLine($"Logger factory is registered as instance: {descriptor.ImplementationInstance != null}");
+              if (descriptor.ServiceType == typeof(ILoggerFactory))
+                        Console.WriteLine($"Logger factory is registered as instance: {descriptor.ImplementationInstance != null}");
 #endif
-            return false; // fallback to default registration logic
+              return false; // fallback to default registration logic
           })
-          
-        // Your registrations are defined in CompositionRoot class
-        .ConfigureServiceProvider<DryIocConfig>();
-    }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseHsts();
-      }
+              // Your registrations are defined in CompositionRoot class
+              .ConfigureServiceProvider<DryIocConfig>();
+        }
 
-      app.UseSignalR(routes =>
-      {
-        routes.MapHub<EventHub>("/events");
-      });
-      app.UseStaticFiles();
-      app.UseHttpsRedirection();
-      app.UseMvc();
-      var migrator = app.ApplicationServices.GetService<IStorageSchemeMigrator>();
-      migrator.CreateIfNotExists();
-      BusConfig.Initialize(app.ApplicationServices.GetService<IBus>());
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+                endpoints.MapHub<EventHub>("/events");
+            });
+
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            var migrator = app.ApplicationServices.GetService<IStorageSchemeMigrator>();
+            migrator.CreateIfNotExists();
+            BusConfig.Initialize(app.ApplicationServices.GetService<IBus>());
+        }
     }
-  }
 }
